b502c4bd0288f42b80b0c8c743d6e00211bea8bd
HYUNWOO CHEON <milcamboo@gmail.com>
2024-01-09 09:37:57 +0900

    [#1] Use dictionary to map number-index and reduce time complexity (330 -> 60ms)

--- array/n_1_two_sum.py
+++ array/n_1_two_sum.py
@@ -5,7 +5,17 @@ class TwoSome:
     @staticmethod
     @time_measurement
     def execute(nums: list[int], target: int) -> list[int]:
-        for idx, num in enumerate(nums):
-            pair_number = target - num
-            if pair_number in nums[idx + 1 :]:
-                return [idx, nums[idx + 1 :].index(pair_number) + idx + 1]
+        # create dictionary to save key-value(number-index)
+        nums_dict = {}
+
+        # iterate list
+        for curr_idx, curr_num in enumerate(nums):
+            # find the number to match the target number
+            pair_numer = target - curr_num
+
+            # return a list if the number exists in the dictionary and its index is not the same as the current number
+            if pair_numer in nums_dict and curr_idx != nums_dict[pair_numer]:
+                return [curr_idx, nums_dict[pair_numer]]
+
+            # if the condition is not met, then save current idx and number in the dictionary
+            nums_dict[curr_num] = curr_idx
---
+++ array/test/test_1.py
@@ -0,0 +1,27 @@
+import unittest
+
+from array.n_1_two_sum import TwoSome
+
+
+class MyTestCase(unittest.TestCase):
+    def test_case_one(self):
+        nums = [2, 7, 11, 15]
+        target = 9
+        expected = [[0, 1], [1, 0]]
+        self.assertIn(TwoSome.execute(nums, target), expected)
+
+    def test_case_two(self):
+        nums = [3, 2, 4]
+        target = 6
+        expected = [[1, 2], [2, 1]]
+        self.assertIn(TwoSome.execute(nums, target), expected)
+
+    def test_case_three(self):
+        nums = [3, 3]
+        target = 6
+        expected = [[0, 1], [1, 0]]
+        self.assertIn(TwoSome.execute(nums, target), expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
