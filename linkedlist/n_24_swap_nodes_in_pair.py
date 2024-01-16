2697fadedbe96a3db6e8854584291fb0817572b4
HYUNWOO CHEON <milcamboo@gmail.com>
2024-01-16 13:39:04 +0900

    [#24] Swap the linked list with recursion

--- linkedlist/n_24_swap_nodes_in_pair.py
+++ linkedlist/n_24_swap_nodes_in_pair.py
@@ -11,15 +11,9 @@ class SwapNodesInPair:
     @staticmethod
     @time_measurement
     def solution(head: ListNode | None) -> ListNode | None:
-        root = prev = ListNode(None)
-        prev.next = head
-        while head and head.next:
-            b = head.next
-            head.next = b.next
-            b.next = head
-
-            prev.next = b
-
-            head = head.next
-            prev = prev.next.next
-        return root.next
+        if head and head.next:
+            p = head.next
+            head.next = SwapNodesInPair.solution(p.next)
+            p.next = head
+            return p
+        return head
