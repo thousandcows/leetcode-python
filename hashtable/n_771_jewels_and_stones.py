950d20b0699e23fad15c225d72cb9984b04bde51
HYUNWOO CHEON <milcamboo@gmail.com>
2024-01-22 10:22:12 +0900

    [#771] Solved using collections.Counter

--- hashtable/n_771_jewels_and_stones.py
+++ hashtable/n_771_jewels_and_stones.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 from utils.time_measurement import time_measurement
 
 
@@ -5,11 +7,4 @@ class JewelsAndStones:
     @staticmethod
     @time_measurement
     def solution(jewels: str, stones: str) -> int:
-        hash = {}
-        for j in jewels:
-            hash[j] = True
-        counter = 0
-        for s in stones:
-            if s in hash:
-                counter += 1
-        return counter
+        return sum(Counter(stones)[j] for j in jewels)
