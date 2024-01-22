f413f62601ca56b1bd120755bbe8f0942cf51106
HYUNWOO CHEON <milcamboo@gmail.com>
2024-01-22 17:46:43 +0900

    [#200] Solved in dfs

--- graphs/n_200_number_of_islands.py
+++ graphs/n_200_number_of_islands.py
@@ -2,30 +2,31 @@ from utils.time_measurement import time_measurement
 
 
 class NumberOfIslands:
-    @staticmethod
+    def dfs(self, grid: list[list[str]], row: int, col: int) -> None:
+        if not (0 <= row < len(grid)) or not (0 <= col < len(grid[0])):
+            return
+
+        if grid[row][col] != "1":
+            return
+
+        grid[row][col] = 0
+
+        self.dfs(grid, row + 1, col)
+        self.dfs(grid, row + -1, col)
+        self.dfs(grid, row, col + 1)
+        self.dfs(grid, row, col + -1)
+
     @time_measurement
-    def solution(grid: list[list[str]]) -> int:
-        length = len(grid)
-        width = len(grid[0])
-        visited = [[False] * width] * length
-        direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-        def bfs(row: int, col: int, visited: list[list[bool]]):
-            visited[row][col] = True
-            for di in direction:
-                n_row, n_col = row + di[0], col + di[1]
-
-                if 0 <= n_row < length and 0 <= n_col < width:
-                    if grid[n_row][n_col] == "1" and not visited[n_row][n_col]:
-                        bfs(n_row, n_col, visited)
+    def solution(self, grid: list[list[str]]) -> int:
+        if not grid:
+            return 0
 
         count = 0
 
-        for r in range(length):
-            for c in range(width):
-                if grid[r][c] == "1" and visited[r][c] is False:
-                    print(f"r: {r}, c: {c}")
-                    bfs(r, c, visited)
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == "1":
+                    self.dfs(grid, r, c)
                     count += 1
 
         return count
--- graphs/test/test_200.py
+++ graphs/test/test_200.py
@@ -4,7 +4,7 @@ from graphs.n_200_number_of_islands import NumberOfIslands
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_case_one(self):
         grid = [
             ["1", "1", "1", "1", "0"],
             ["1", "1", "0", "1", "0"],
@@ -12,9 +12,9 @@ class MyTestCase(unittest.TestCase):
             ["0", "0", "0", "0", "0"],
         ]
         output = 1
-        self.assertEqual(output, NumberOfIslands.solution(grid))  # add assertion here
+        self.assertEqual(output, NumberOfIslands().solution(grid))  # add assertion here
 
-    def test_something2(self):
+    def test_case_two(self):
         grid = [
             ["1", "1", "0", "0", "0"],
             ["1", "1", "0", "0", "0"],
@@ -22,12 +22,22 @@ class MyTestCase(unittest.TestCase):
             ["0", "0", "0", "1", "1"],
         ]
         output = 3
-        self.assertEqual(output, NumberOfIslands.solution(grid))
+        self.assertEqual(output, NumberOfIslands().solution(grid))
 
-    def test_something3(self):
+    def test_case_three(self):
         grid = [["0", "1", "0"], ["1", "0", "1"], ["0", "1", "0"]]
         output = 4
-        self.assertEqual(output, NumberOfIslands.solution(grid))
+        self.assertEqual(output, NumberOfIslands().solution(grid))
+
+    def test_when_it_is_the_whole_island(self):
+        grid = [["1", "1", "1"], ["1", "0", "1"], ["1", "1", "1"]]
+        output = 1
+        self.assertEqual(output, NumberOfIslands().solution(grid))
+
+    def test_when_there_is_no_island(self):
+        grid = [["0", "0", "0"], ["0", "0", "0"], ["0", "0", "0"]]
+        output = 0
+        self.assertEqual(output, NumberOfIslands().solution(grid))
 
 
 if __name__ == "__main__":
