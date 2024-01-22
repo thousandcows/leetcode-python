b1129d37e2d8e8a5b243c16cedb367191bb56c0b
HYUNWOO CHEON <milcamboo@gmail.com>
2024-01-22 17:53:07 +0900

    [#200] Refactored to nested function to remove self and let the function use the same list without passing them as parameter

--- graphs/n_200_number_of_islands.py
+++ graphs/n_200_number_of_islands.py
@@ -2,31 +2,28 @@ from utils.time_measurement import time_measurement
 
 
 class NumberOfIslands:
-    def dfs(self, grid: list[list[str]], row: int, col: int) -> None:
-        if not (0 <= row < len(grid)) or not (0 <= col < len(grid[0])):
-            return
-
-        if grid[row][col] != "1":
-            return
-
-        grid[row][col] = 0
-
-        self.dfs(grid, row + 1, col)
-        self.dfs(grid, row + -1, col)
-        self.dfs(grid, row, col + 1)
-        self.dfs(grid, row, col + -1)
-
     @time_measurement
     def solution(self, grid: list[list[str]]) -> int:
-        if not grid:
-            return 0
+        def dfs(row: int, col: int) -> None:
+            if not (0 <= row < len(grid)) or not (0 <= col < len(grid[0])):
+                return
+
+            if grid[row][col] != "1":
+                return
+
+            grid[row][col] = 0
+
+            dfs(row + 1, col)
+            dfs(row + -1, col)
+            dfs(row, col + 1)
+            dfs(row, col + -1)
 
         count = 0
 
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 if grid[r][c] == "1":
-                    self.dfs(grid, r, c)
+                    dfs(r, c)
                     count += 1
 
         return count
