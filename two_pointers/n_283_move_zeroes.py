2557ca6786002d1fd1e275c904fb07d7b755c1ff
thousandcows <milcamboo@gmail.com>
2024-02-21 12:34:28 +0900

    [#283] use two pointers in more efficient way

--- two_pointers/n_283_move_zeroes.py
+++ two_pointers/n_283_move_zeroes.py
@@ -5,12 +5,17 @@ class MoveZeroes:
     @staticmethod
     @time_measurement
     def solution(nums):
-        left, right = 0, len(nums) - 1
-        while left < right:
-            if nums[left] == 0:
-                nums.remove(0)
-                nums.append(0)
-                right -= 1
-                continue
-            left += 1
+        zero_idx, non_zero_idx = 0, 0
+        count_zero = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                count_zero += 1
+            else:
+                nums[non_zero_idx] = nums[i]
+                non_zero_idx += 1
+
+        for i in range(len(nums) - 1, len(nums) - count_zero - 1, -1):
+            nums[i] = 0
+
         return nums
+
