1d0b575ba9ecb00dec4796047d76924311233d73
thousandcows <milcamboo@gmail.com>
2024-02-23 13:40:17 +0900

    [#643] optimized the solution by accessing only two elements at a time

--- sliding_window/n_643_maximum_average_subarray_one.py
+++ sliding_window/n_643_maximum_average_subarray_one.py
@@ -7,7 +7,9 @@ class MaximumAverageSubarrayOne:
     @staticmethod
     @time_measurement
     def solution(nums: list[int], k: int) -> float:
-        answer = -math.inf
-        for i in range(len(nums) - k + 1):
-            answer = max(answer, sum(nums[i:i + k]))
+        window_sum = sum(nums[:k])
+        answer = window_sum
+        for i in range(1, len(nums) - k + 1):
+            window_sum = window_sum - nums[i - 1] + nums[i + k - 1]
+            answer = max(answer, window_sum)
         return round(answer / k, 5)
