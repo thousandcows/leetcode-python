d1ae3dc84c69b9a13f210451826c6cfc79462b72
HYUNWOO CHEON <milcamboo@gmail.com>
2024-03-28 18:11:43 +0900

    [#1365] solved with hashtable

---
+++ hashtable/n_1365_how_many_numbers_are_smaller_than_the_current_number.py
@@ -0,0 +1,27 @@
+from collections import Counter, defaultdict
+
+from utils.time_measurement import time_measurement
+
+
+class HowManyNumbersAreSmallerThanTheCurrentNumber:
+    @staticmethod
+    @time_measurement
+    def solution(nums: list[int]) -> list[int]:
+        answer = []
+        hash_table = dict(Counter(nums))
+        sorted_hash_keys = sorted(list(hash_table.keys()))
+
+        memo_dict = defaultdict(int)
+        for n in nums:
+            if n in memo_dict:
+                answer.append(memo_dict[n])
+                continue
+
+            cnt = 0
+            for idx, key in enumerate(sorted_hash_keys):
+                if key < n:
+                    cnt += hash_table[key]
+            memo_dict[n] += cnt
+
+            answer.append(cnt)
+        return answer
---
+++ hashtable/test/test_1365.py
@@ -0,0 +1,32 @@
+import unittest
+
+from hashtable.n_1365_how_many_numbers_are_smaller_than_the_current_number import (
+    HowManyNumbersAreSmallerThanTheCurrentNumber,
+)
+
+
+class MyTestCase(unittest.TestCase):
+    def test_case_one(self):
+        nums = [8, 1, 2, 2, 3]
+        expected = [4, 0, 1, 1, 3]
+        self.assertEqual(
+            expected, HowManyNumbersAreSmallerThanTheCurrentNumber.solution(nums)
+        )  # add assertion here
+
+    def test_case_two(self):
+        nums = [6, 5, 4, 8]
+        expected = [2, 1, 0, 3]
+        self.assertEqual(
+            expected, HowManyNumbersAreSmallerThanTheCurrentNumber.solution(nums)
+        )
+
+    def test_case_three(self):
+        nums = [7, 7, 7, 7]
+        expected = [0, 0, 0, 0]
+        self.assertEqual(
+            expected, HowManyNumbersAreSmallerThanTheCurrentNumber.solution(nums)
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
