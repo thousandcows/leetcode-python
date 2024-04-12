39049851c4bfc6a965ee1e7f12207f59530c46e1
HYUNWOO CHEON <milcamboo@gmail.com>
2024-04-12 13:36:38 +0900

    [#67] solve with built-in functions

--- simulation/n_67_add_binary.py
+++ simulation/n_67_add_binary.py
@@ -5,11 +5,4 @@ class AddBinary:
     @staticmethod
     @time_measurement
     def solution(a: str, b: str):
-        def change_to_decimal(binary: str):
-            decimal = 0
-            for i in range(len(binary) - 1, -1, -1):
-                decimal += pow(2, i) if binary[len(binary) - 1 - i] == "1" else 0
-
-            return decimal
-
-        return bin(change_to_decimal(a) + change_to_decimal(b))[2:]
+        return bin(int(a, 2) + int(b, 2))[2:]
