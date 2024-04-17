097689801e09e96b91fde2cad7edfa9738b08afb
HYUNWOO CHEON <milcamboo@gmail.com>
2024-04-17 13:57:31 +0900

    [#498] Add comments for the future review

--- simulation/n_498_diagonal_traverse.py
+++ simulation/n_498_diagonal_traverse.py
@@ -5,11 +5,16 @@ class DiagonalTraverse:
     @staticmethod
     @time_measurement
     def solution(mat: list[list[int]]) -> list[int]:
-        ans = [mat[0][0]]
         row, col = 0, 0
         m, n = len(mat), len(mat[0])
 
+        # The traverse always starts with the first element
+        ans = [mat[0][0]]
+
+        # Iterate while both row and col are not at the end of the matrix
         while not (row == m - 1 and col == n - 1):
+            # Move towards the left-bottom corner
+            # Decide where to move next
             if 0 <= row < m and 0 <= col + 1 < n:
                 col += 1
             elif 0 <= row + 1 < m and 0 <= col < n:
@@ -26,6 +31,8 @@ class DiagonalTraverse:
                 row += 1
                 col -= 1
 
+            # Move towards the right-top corner
+            # Decide where to move next
             if 0 <= row + 1 < m and 0 <= col < n:
                 row += 1
             elif 0 <= row < m and 0 <= col + 1 < n:
