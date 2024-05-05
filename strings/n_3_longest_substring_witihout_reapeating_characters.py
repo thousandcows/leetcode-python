5c201264c6ea48d81ad1387f417667ef7497a5e5
thousandcows <milcamboo@gmail.com>
2024-05-05 19:46:04 +0900

    [#3] improved performance using index only

--- strings/n_3_longest_substring_witihout_reapeating_characters.py
+++ strings/n_3_longest_substring_witihout_reapeating_characters.py
@@ -5,23 +5,12 @@ class LongestSubstringWithoutRepeatingCharacters:
     @staticmethod
     @time_measurement
     def solution(s: str) -> int:
-        if len(s) <= 1:
-            return len(s)
-
-        answer, left, right = 0, 0, 0
-        curr_substr_set = set()
-
-        while right < len(s):
-            if left == right:
-                curr_substr_set.add(s[left])
-                right += 1
-            elif left < right:
-                if s[right] not in curr_substr_set:
-                    curr_substr_set.add(s[right])
-                    right += 1
-                else:
-                    left += 1
-                    right = left
-                    curr_substr_set = set(s[left])
-            answer = max(answer, right - left)
+        answer, start_index = 0, 0
+        visited = {}
+        for idx, char in enumerate(s):
+            if char in visited and start_index <= visited[char]:
+                start_index = visited[char] + 1
+            else:
+                answer = max(answer, idx - start_index + 1)
+            visited[char] = idx
         return answer
